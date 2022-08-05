c25036cf037e0e8d6516d95b55d8287520657ef5
Dmitry Vakin <d.vakin@procsy.ru>
2022-08-05 22:02:55 +0300

    Fixed module style for burger-ingredients

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -2,15 +2,14 @@ import TabContainer from "../tab-container/tab-container";
 import IngredientCard from "../ingredient-card/ingredient-card";
 import ingredients from "../../mocks/ingredients";
 import { INGREDIENT_TYPES } from "../../constants";
+import { getClassName } from "../../utils";
 
-import "./burger-ingredients.css";
+import style from "./burger-ingredients.module.css";
 
 function BurgerIngredients(props) {
   return (
-    <div className="burger-ingredients-wrapper ingredients mt-10">
-      <h2 className="ingredients__title text text_type_main-large mb-5">
-        Соберите бургер
-      </h2>
+    <div className={getClassName(style["burger-ingredients-wrapper"], "mt-10")}>
+      <h2 className="text text_type_main-large mb-5">Соберите бургер</h2>
       <TabContainer />
 
       <div className="groups">
@@ -18,7 +17,7 @@ function BurgerIngredients(props) {
           <h3 className="group__title text text_type_main-medium mb-6">
             Булки
           </h3>
-          <div className="ingredients__list">
+          <div className={style["ingredients__list"]}>
             {ingredients
               .filter((item) => item.type === INGREDIENT_TYPES.BUN)
               .map((item) => (
@@ -33,10 +32,8 @@ function BurgerIngredients(props) {
         </div>
 
         <div className="group mb-10">
-          <h3 className="group__title text text_type_main-medium mb-6">
-            Соусы
-          </h3>
-          <div className="ingredients__list">
+          <h3 className="text text_type_main-medium mb-6">Соусы</h3>
+          <div className={style["ingredients__list"]}>
             {ingredients
               .filter((item) => item.type === INGREDIENT_TYPES.SAUCE)
               .map((item) => (
@@ -51,10 +48,8 @@ function BurgerIngredients(props) {
         </div>
 
         <div className="group mb-10">
-          <h3 className="group__title text text_type_main-medium mb-6">
-            Начинка
-          </h3>
-          <div className="ingredients__list">
+          <h3 className="text text_type_main-medium mb-6">Начинка</h3>
+          <div className={style["ingredients__list"]}>
             {ingredients
               .filter((item) => item.type === INGREDIENT_TYPES.MAIN)
               .map((item) => (
